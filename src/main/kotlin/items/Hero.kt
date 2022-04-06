b35b9762ae2c6db28855b0442cbcc05d98524032
Marco <gdmarav374@gmail.com>
2022-04-05 23:13:54 -0500

    Make hero capable of turning

--- src/main/kotlin/items/Hero.kt
+++ src/main/kotlin/items/Hero.kt
@@ -6,4 +6,21 @@ class Hero(initialPosition: Coordinates, power: Int) : Character(initialPosition
     override fun getLogo(): String {
         return direction.logo
     }
+
+    fun displayCurrentStats() {
+        print("hero power: $power")
+        println("hero position: $curentPosition")
+    }
+
+    fun turnLeft() {
+        this.direction = direction.left
+    }
+
+    fun turnRight() {
+        this.direction = direction.right
+    }
+
+    fun rotate180() {
+        this.direction = direction.opposite
+    }
 }
