fd02d0aa40774e3bfadbd680226923dc70d71286
Marco <gdmarav374@gmail.com>
2022-04-05 23:12:35 -0500

    Improve direction enum

--- src/main/kotlin/items/Direction.kt
+++ src/main/kotlin/items/Direction.kt
@@ -6,24 +6,29 @@ enum class Direction(val logo: String, val xForward: Int, val yForward: Int) {
     E("►", 1, 0),
     W("◄", -1, 0);
 
-    private var left: Direction? = null
-    private var right: Direction? = null
-    private var opposite: Direction? = null
-
-    companion object {
-        init {
-            N.left = W
-            S.left = E
-            E.left = N
-            W.left = S
-            N.right = E
-            S.right = W
-            E.right = S
-            W.right = N
-            N.opposite = S
-            S.opposite = N
-            E.opposite = W
-            W.opposite = E
+    val left: Direction
+        get() = when (this) {
+            N -> W
+            S -> E
+            E -> N
+            W -> S
         }
-    }
+
+    val right: Direction
+        get() = when (this) {
+            N -> E
+            S -> W
+            E -> S
+            W -> N
+        }
+
+    val opposite: Direction
+        get() = when (this) {
+            N -> S
+            S -> N
+            E -> W
+            W -> E
+        }
+
+
 }
